d6ffda066f0ce3500fed34e581dd042776ea645d
Simon Barak <barak.simon.as@gmail.com>
2024-01-24 12:50:13 +0100

    hero

--- components/hero.js
+++ components/hero.js
@@ -4,7 +4,7 @@ const Hero = ({ title, subtitle }) => {
   return (
     <section className="h-half flex items-center">
       <div className="max-w-2xl 2xl:max-w-4xl mx-auto text-gray-700 px-4">
-        <h1 className="text-2xl lg:text-4xl 2xl:text-6xl mb-8">
+        <h1 className="text-2xl lg:text-4xl 2xl:text-5xl mb-8 2xl:mb-10">
           <p>
             {title.split("\\n").map((line, i) => (
               <React.Fragment key={i}>
@@ -16,7 +16,7 @@ const Hero = ({ title, subtitle }) => {
         </h1>
 
         {subtitle && (
-          <h2 className="text-lg lg:text-xl 2xl:text-2xl max-w-xl text-gray-700">
+          <h2 className="text-lg lg:text-xl 2xl:text-2xl max-w-xl 2xl:max-w-2xl text-gray-700">
             <p>
               {subtitle.split("\\n").map((line, i) => (
                 <React.Fragment key={i}>
