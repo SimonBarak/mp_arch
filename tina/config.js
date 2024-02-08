0a353f8fe55af8f6082d2547bba5aa29d8449d55
Simon Barak <barak.simon.as@gmail.com>
2024-02-08 15:24:19 +0100

    cloudinary + netlify

--- tina/config.js
+++ tina/config.js
@@ -18,16 +18,6 @@ export const config = defineConfig({
   //process.env.HEAD, // Netlify branch env
   token: "faa28d2ec1cb3b18b9eb04b76e7db34c387e7317",
   media: {
-    // If you wanted cloudinary do this
-    // loadCustomStore: async () => {
-    //   const pack = await import("next-tinacms-cloudinary");
-    //   return pack.TinaCloudCloudinaryMediaStore;
-    // },
-    // this is the config for the tina cloud media store
-    // tina: {
-    //   publicFolder: "public",
-    //   mediaRoot: "uploads",
-    // },
     loadCustomStore: async () => {
       const pack = await import("next-tinacms-cloudinary");
       return pack.TinaCloudCloudinaryMediaStore;
